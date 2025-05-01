ca9d2109b1f237002d21619767fd18d9e8d46934
Mohammad Ahmad <m.a.7snen@gmail.com>
2025-05-01 03:48:46 +0300

    Updated CORS

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://178.128.90.137"],  # Allows all origins
+    allow_origins=["http://178.128.90.137, https://front-genta.xyz"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Allows all methods
     allow_headers=["Content-Type", "Authorization"],  # Allows all headers
