dedbd0843f42330ee254cecb82d1e7ba39704241
Mohammad Ahmad <m.a.7snen@gmail.com>
2025-05-01 03:08:07 +0300

    Update CORS

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Allows all methods
+    allow_headers=["Content-Type", "Authorization"],  # Allows all headers
 )
 
 # Create database tables
